bd0548f252f2322e8066b067ce923596dfc7f845
13thm <83104778+13thm@users.noreply.github.com>
2024-01-13 17:49:35 +0800

    初试微服务8001端口的微服务

---
+++ cloud-provider-payment8001/src/main/java/org/example/springcloud/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,32 @@
+package org.example.springcloud.service.impl;
+
+import org.example.springcloud.dao.PaymentDao;
+import org.example.springcloud.entities.Payment;
+import org.example.springcloud.service.PaymentService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * @auther zzyy
+ * @create 2020-01-27 21:12
+ */
+@Service
+public class PaymentServiceImpl implements PaymentService
+{
+    @Resource
+    private PaymentDao paymentDao;
+
+
+    @Override
+    public int create(Payment payment)
+    {
+        return paymentDao.create(payment);
+    }
+
+    @Override
+    public Payment getPaymentById(Long id)
+    {
+        return paymentDao.getPaymentById(id);
+    }
+}
