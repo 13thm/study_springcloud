fd0b3dfbeebd37eaea1f5538bf6b5ba34a1cc337
13thm <83104778+13thm@users.noreply.github.com>
2024-01-17 01:28:11 +0800

    Add files via upload

---
+++ days2/cloud-provider-payment8001/src/main/java/org/example/springcloud/service/PaymentService.java
@@ -0,0 +1,19 @@
+package org.example.springcloud.service;
+
+
+import org.apache.ibatis.annotations.Param;
+import org.example.entities.Payment;
+
+
+/**
+ * @auther zzyy
+ * @create 2020-01-27 21:11
+ */
+public interface PaymentService
+{
+    public int create(Payment payment);
+
+
+    public Payment getPaymentById(@Param("id") Long id);
+
+}
