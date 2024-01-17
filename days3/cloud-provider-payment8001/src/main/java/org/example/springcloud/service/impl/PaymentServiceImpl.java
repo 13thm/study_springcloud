37425c9147f0be274d205b2825b62d1025f957a3
13thm <83104778+13thm@users.noreply.github.com>
2024-01-17 17:35:45 +0800

    Add files via upload
    第三个eureka项目的学习

---
+++ days3/cloud-provider-payment8001/src/main/java/org/example/springcloud/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,30 @@
+package org.example.springcloud.service.impl;
+import org.example.springcloud.dao.PaymentDao;
+import org.example.springcloud.service.PaymentService;
+import org.springframework.stereotype.Service;
+import org.example.entities.Payment;
+import javax.annotation.Resource;
+
+/**
+ * @auther zzyy
+ * @create 2020-01-27 21:12
+ */
+@Service
+public class PaymentServiceImpl implements PaymentService
+{
+    @Resource
+    private PaymentDao paymentDao;
+
+
+    @Override
+    public int create(Payment payment)
+    {
+        return paymentDao.create(payment);
+    }
+
+    @Override
+    public Payment getPaymentById(Long id)
+    {
+        return paymentDao.getPaymentById(id);
+    }
+}
